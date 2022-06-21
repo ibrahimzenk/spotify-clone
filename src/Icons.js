df804dc0978e25a3e4ac318f1f4f2af691b1619b
İbrahim Zenk <ibrahimzenk@hotmail.com>
2022-06-21 22:09:03 +0300

    Icons Added

--- src/Icons.js
+++ src/Icons.js
@@ -59,6 +59,39 @@ const DownloadIcon = ({ size }) => {
   );
 };
 
+const PrevIcon = ({ size }) => {
+  return (
+    <svg role="img" viewBox="0 0 24 24" height={size} width={size}>
+      <path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z" fill="#fff" />
+    </svg>
+  );
+};
+
+const NextIcon = ({ size }) => {
+  return (
+    <svg role="img" viewBox="0 0 24 24" height={size} width={size}>
+      <path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z" fill="#fff" />
+    </svg>
+  );
+};
+
+const DownDirIcon = ({ size }) => {
+  return (
+    <svg role="img" height={size} width={size} viewBox="0 0 16 16">
+      <path d="M14 6l-6 6-6-6h12z" fill="#fff" />
+    </svg>
+  );
+};
+
+const ExternalIcon = ({ size }) => {
+  return (
+    <svg role="img" height={size} width={size} viewBox="0 0 16 16">
+      <path d="M1 2.75A.75.75 0 011.75 2H7v1.5H2.5v11h10.219V9h1.5v6.25a.75.75 0 01-.75.75H1.75a.75.75 0 01-.75-.75V2.75z" fill="#fff" />
+      <path d="M15 1v4.993a.75.75 0 11-1.5 0V3.56L8.78 8.28a.75.75 0 01-1.06-1.06l4.72-4.72h-2.433a.75.75 0 010-1.5H15z" fill="#fff" />
+    </svg>
+  );
+};
+
 const Icon = ({ name, size = 24 }) => {
   const icons = {
     home: HomeIcon,
@@ -67,6 +100,10 @@ const Icon = ({ name, size = 24 }) => {
     plus: PlusIcon,
     heart: HeartIcon,
     download: DownloadIcon,
+    prev: PrevIcon,
+    next: NextIcon,
+    downDir: DownDirIcon,
+    external: ExternalIcon,
   };
 
   const Component = icons[name];
