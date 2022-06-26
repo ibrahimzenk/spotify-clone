6f7e936115cdbe218c67fa785954b8058dd31d6f
İbrahim Zenk <ibrahimzenk@hotmail.com>
2022-06-26 18:46:13 +0300

    line-clamp added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         backdrop: "#121212",
-        primary: "1db954",
+        primary: "#1db954",
         active: "#282828",
         link: "#b3b3b3",
         footer: "#181818",
@@ -15,5 +15,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
